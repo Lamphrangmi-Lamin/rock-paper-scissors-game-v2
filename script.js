56dcb7fb743e1c83cca9b2484e769695c15ea33a
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2022-08-18 20:29:56 +0530

    adding  a simple ui buttons

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-// function generating random value rock/paper/scissor 
+// function generating random value rock/paper/scissor
 function computerPlay() {
     let choice = ['Rock', 'Paper', 'Scissors'];
     let random = Math.floor(Math.random() * choice.length);
     return choice[random];
 }
 
-//function for one round execution
-function playRound(playerSelection, computerSelection) {
-    playerSelection;
-    computerSelection;
+// initializing score values
+let usrScore = parseInt(0);
+let computerScore = parseInt(0);
 
-    if (computerSelection === playerSelection) {
-        console.log("It's a tie.")
-    } else if (playerSelection === 'Rock') {
-        if (computerSelection === 'Paper') {
-            console.log("You Lose!! Paper Beats Rock");
-            computerScore+= 1;
-        } else {
-            console.log("Rock beats Scissors. You Won!!");
-            usrScore+= 1;
+const weapons = document.querySelectorAll("button");
+for (let i = 0; i < weapons.length; i++) {
+    weapons[i].addEventListener('click', () => {
+        computerPlay();
+        
+        if (computerPlay() === weapons[i].value) {
+            result.textContent = "It's a tie."
+        } else if (weapons[i].value === 'Rock') {
+            if (computerPlay() === 'Paper') {
+                result.textContent = "You Lose!! Paper Beats Rock";
+                computerScore += 1;
+            } else {
+                result.textContent = "Rock beats Scissors. You Won!!";
+                usrScore += 1;
+            }
+        } else if (weapons[i].value === 'Paper') {
+            if (computerPlay() === 'Rock') {
+                result.textContent = "Paper beats rock. You Won!!";
+                usrScore += 1;
+            } else {
+                result.textContent = "Scissors beats paper. You Lose!!";
+                computerScore += 1;
+            }
+        } else if (weapons[i].value === 'Scissors') {
+            if (computerPlay() === 'Paper') {
+                result.textContent = "Scissors beats paper. You Won!!";
+                usrScore += 1;
+            } else {
+                result.textContent = "Rock beats scissors. You Lose!!";
+                computerScore += 1;
+            }
         }
-    } else if (playerSelection === 'Paper') {
-        if (computerSelection === 'Rock') {
-            console.log("Paper beats rock. You Won!!");
-            usrScore+= 1;
-        } else {
-            console.log("Scissors beats paper. You Lose!!");
-            computerScore+= 1;
+        else {
+            result.textContent = "Wrong Input!";
         }
-    } else if (playerSelection === 'Scissors') {
-        if (computerSelection === 'Paper') {
-            console.log("Scissors beats paper. You Won!!");
-            usrScore+= 1;
-        } else {
-            console.log("Rock beats scissors. You Lose!!");
-            computerScore+= 1;
-        }
-    } else {
-        console.log("Wrong Input!");
-    }
+            
+    });
 }
-
-// function for prompting the user for an input
-function userPlay() {
-    return prompt("Rock/paper/scissors??");
-}
-
-//initializing score values
-let usrScore = 0;
-let computerScore = 0;
 
 // function that will run the game
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(userPlay(), computerPlay());
-        if (i == 4) {
-            if (usrScore > computerScore) {
-                console.log(`You won the game with a score of ${usrScore}`);
-            } else if (usrScore == computerScore) {
-                console.log("NOBODY WINS. Everybody has an equal number of scores.");
-            } else if (usrScore < computerScore) {
-                console.log(`Computer is the final winner with a score of ${computerScore}`);
-            }
-            console.log(usrScore);
-            console.log(computerScore);
-        } else {
-            playRound(userPlay(), computerPlay());
-        }
-    }
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         playRound(userPlay(), computerPlay());
+//         if (i == 4) {
+//             if (usrScore > computerScore) {
+//                 winner.textContent = `You won the game with a score of ${usrScore}`;
+//             } else if (usrScore == computerScore) {
+//                 winner.textContent = "NOBODY WINS. Everybody has an equal number of scores.";
+//             } else if (usrScore < computerScore) {
+//                 winner.textContent = `Computer is the final winner with a score of ${computerScore}`;
+//             }
+//             userpoints.textContent(usrScore);
+//             computerpoints.textContent(computerScore);
+//         } else {
+//             playRound(userPlay(), computerPlay());
+//         }
+//     }
+// }
 
 // functionn to reset the game
 function reset() {
@@ -76,5 +74,11 @@ function reset() {
     computerScore = 0;
 }
 
-// calling the game() to run
-game();
+const body = document.querySelector("body");
+body.style.background = "grey";
+
+
+const result = document.getElementById('result');
+const winner = document.getElementById('winner');
+const userpoints = document.getElementById('userpoints');
+const computerpoints = document.getElementById('computerpoints');
